999f555ffa2fcca1e4e58803321758fd5589bef7
Kevin Finisterre <Mavproxyuser>
2020-12-02 22:25:33 -0500

    print likely confirmed mateA & mateB matches

--- leafly_scrape.rb
+++ leafly_scrape.rb
@@ -23,20 +23,35 @@ numpages.times { |digit|
 
 	parsed = JSON.load(finalpage)
 	parsed['props']['pageProps']['strains'].each do | strain |
-		puts "https://www.leafly.com/strains/" + strain['slug']
-	#	puts strain['category']
-	#	puts strain['chemotype']
-	#	puts strain['description']
 		x =strain['description'].split(/\s+/).find_all { |u| u =~ /^href?/ }
-	 	x.each do | parent |
-			if parent.include? "/strains/"
-				if parent.include? "/strains/lists"
-				else 
-					puts "https://www.leafly.com/strains" + parent.split('"')[1]
+		if x.size == 0
+		#	puts "not proper number of elements: " + x.size.to_s	
+    
+		elsif x.size == 2
+		#	puts strain['category']
+		#	puts strain['chemotype']
+		#	puts strain['description']
+			
+			output = []
+			output << "-> https://www.leafly.com/strains/" + strain['slug']
+
+	 		x.each do | parent |
+				if parent.include? "/strains/"
+					if parent.include? "/strains/lists"
+					else 
+						output << "`---> https://www.leafly.com/strains" + parent.split('"')[1]
+					end
 				end
 			end
+			
+			if output.size == 3
+				puts output
+				puts "--------------------------------------------------------------"
+			end
+		else
+		#	puts "not proper number of elements: " + x.size.to_s	
+
 		end
-	
-		puts "---------------------------------------------------"
+		
 	end	
 }
